18875a137b4600497302bdb884ca98ef6ead7f8f
jlafer <jlafer@twilio.com>
2019-03-30 22:41:27 -0700

    added most ioutil functions from avconnect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,81 @@
 const {fromPairs} = require('ramda');
+const fs = require('fs');
+
+function makeDir(path, mask) {
+  return new Promise(function(resolve, reject) {
+    fs.mkdir(path, mask, function(err) {
+        if (err)
+          if (err.code == 'EEXIST')
+            resolve(null);
+          else
+            reject(err);
+        else
+          resolve(path);
+    });
+  });
+}
+
+async function ensurePathExists(parentPath, mask, ...pathNodes) {
+  let path = parentPath;
+  for (let node of pathNodes) {
+    path += ('/' + node);
+    await makeDir(path, mask);
+  }
+  return path;
+}
+
+function readTextFile(path) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(path, 'utf8',
+      function(err, text) {
+        if (err) reject(err);
+        resolve(text);
+      }
+    );
+  });
+}
+
+function readJsonFile(path) {
+  return readTextFile(path)
+  .then(function(text) {
+    return new Promise(function(resolve, reject) {
+      let data;
+      try {
+        data = JSON.parse(text);
+      }
+      catch (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  })
+}
+
+function readJsonFiles(dir, filenames) {
+  return Promise.all(filenames.map(filename => readJsonFile(dir + filename)));
+}
+
+/*
+  writeToTextFile assumes that directories in path already exist.
+  If not, first call ensurePathExists().
+*/
+function writeToTextFile(path, str) {
+  return new Promise(function(resolve, reject) {
+    fs.writeFile(path, str,
+      function(err) {
+        if (err) reject(err);
+        resolve(null);
+      }
+    );
+  });
+}
+
+const writeToBinaryFile = writeToTextFile;
+
+function writeToJsonFile(path, value) {
+  const str = JSON.stringify(value, null, 2);
+  return writeToTextFile(path, str);
+}
 
 const argToKVPairs = (arg, idx) => {
   const re = /([^=]+)[=](.+)/;
@@ -19,6 +96,13 @@ const getBaseAxiosConfig = (baseURL, username, password) => ({
 });
 
 module.exports = {
+  ensurePathExists,
+  readTextFile,
+  readJsonFile,
+  readJsonFiles,
+  writeToTextFile,
+  writeToJsonFile,
+  writeToBinaryFile,
   argvToDict,
   getBaseAxiosConfig
 }
