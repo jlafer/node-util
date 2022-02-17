ae51869e8502e85561231965229efdeda9173eac
John Lafer <jlafer@yahoo.com>
2022-02-16 17:24:51 -0800

    added checkDirExists and copyTextFile

--- src/index.js
+++ src/index.js
@@ -134,6 +134,29 @@ function writeToJsonFile(path, value) {
   return writeToTextFile(path, str);
 }
 
+const copyTextFile = (indir, outdir, filename) => {
+  const inPath = `${indir}/${filename}`;
+  const outPath = `${outdir}/${filename}`;
+  readTextFile(inPath)
+  .then((data) => {
+    //console.log(`data read from ${inPath}`);
+    writeToTextFile(outPath, data)
+  })
+};
+
+function checkDirExists(path) {
+  return new Promise(function(resolve, _reject) {
+    fs.stat(
+      path,
+      function(err, stats) {
+        if (err)
+          resolve(false);
+        resolve(true);
+      }
+    );
+  });
+}
+
 const argToKVPairs = (arg, idx) => {
   const re = /([^=]+)[=](.+)/;
   const found = arg.match(re);
@@ -153,6 +176,8 @@ const getBaseAxiosConfig = (baseURL, username, password) => ({
 });
 
 module.exports = {
+  checkDirExists,
+  copyTextFile,
   ensurePathExists,
   openFile,
   writeToFile,
